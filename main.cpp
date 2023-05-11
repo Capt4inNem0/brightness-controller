3c8384a0081029ec4b7e9337a6d00b90900b93dc
Capt4inNem0 <gastoncollo@gmail.com>
2023-05-11 03:44:57 -0300

    [UPD] Handling argv cases
    [UPD] Adjusted min/max value limits

--- main.cpp
+++ main.cpp
@@ -4,14 +4,31 @@
 
 int main(int argc, const char * argv[]) {
 
-    if (argc != 4) {
-        std::cout << "Usage: ./brightness <filename> [sum|set|minus] <brightness-level%>" << std::endl;
+    std::string filename;
+    std::string operation;
+    std::string brightness_level = "100";
+
+    if (argc < 3 || argc > 4) {
+        std::cout << "Usage: ./brightness <filename> [sum|set|minus|get] <brightness-level%>" << std::endl;
         return 1;
+    } else {
+        filename = argv[1];
+        operation = argv[2];
+        if (argc == 4) {
+            if (std::string(argv[2]) == "get") {
+                std::cout << "Usage: ./brightness <filename> [sum|set|minus|get] <brightness-level%>" << std::endl;
+                return 1;
+            }
+            brightness_level = argv[3];
+        } else {
+            if (std::string(argv[2]) != "get") {
+                std::cout << "Usage: ./brightness <filename> [sum|set|minus|get] <brightness-level%>" << std::endl;
+                return 1;
+            }
+        }
     }
 
-    std::string filename = argv[1];
-    std::string operation = argv[2];
-    std::string brightness_level = argv[3];
+
     FILE * max_brightness = fopen((filename + "/max_brightness").c_str(), "r");
     FILE * brightness = fopen((filename + "/brightness").c_str(), "r");
     if (brightness == nullptr || max_brightness == nullptr) {
@@ -35,23 +52,25 @@ int main(int argc, const char * argv[]) {
     fclose(max_brightness);
     fclose(brightness);
 
-    int target_value = -1;
+    int value_range = max_brightness_value * (1 - 0.05);
+    int capped_min_value = max_brightness_value * 0.05;
+
+    int target_value;
     if (operation == "sum") {
-        target_value = brightness_value + std::stoi(brightness_level) * max_brightness_value / 100;
+        target_value = brightness_value + std::stoi(brightness_level) * value_range / 100;
     } else if (operation == "set") {
         target_value = std::stoi(brightness_level) * max_brightness_value / 100;
     } else if (operation == "minus") {
-        target_value = brightness_value - std::stoi(brightness_level) * max_brightness_value / 100;
+        target_value = brightness_value - std::stoi(brightness_level) * value_range / 100;
+    } else if (operation == "get") {
+        std::cout << brightness_value * 100 / max_brightness_value << std::endl;
+        return 0;
     } else {
         std::cerr << "Invalid operation" << std::endl;
         return 1;
     }
-    int f_percent = max_brightness_value * 0.05;
-    if (target_value < f_percent) {
-        target_value = f_percent;
-    } else if (target_value > max_brightness_value) {
-        target_value = max_brightness_value;
-    }
+
+    target_value = std::max(std::min(target_value, max_brightness_value), capped_min_value)
 
     FILE * brightness_output = fopen((filename + "/brightness").c_str(), "w");
     if (brightness_output == nullptr) {
@@ -60,7 +79,6 @@ int main(int argc, const char * argv[]) {
         } else {
             std::cerr << "Error opening file." << std::endl;
         }
-
         return 1;
     }
     fwrite(std::to_string(target_value).c_str(), sizeof(char), std::to_string(target_value).length(), brightness_output);
